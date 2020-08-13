cfe136c78ebe22959aec4e6a82f6343032237737
taro.kirameki <taro.kirameki@techacademy.jp>
2020-08-13 01:56:44 +0000

    CRUD tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,55 @@
+class TasksController < ApplicationController
+  def index
+    @task = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    
+    if @task.save
+      flash[:success] = 'タスクが投稿されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'タスクが投稿されません'
+      render :new
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+     @task = Task.find(params[:id])
+
+  if @task.update(task_params)
+    flash[:success] = 'タスクが更新されました'
+    redirect_to @task
+  else
+    flash.now[:danger] = 'タスクが更新されませんでした'
+    render :new
+  end
+  end
+
+  def destroy
+    @message = Message.find(params[:id])
+    @message.destroy
+
+    flash[:success] = 'Message は正常に削除されました'
+    redirect_to tasks_url
+  end
+end
+
+  private
+  
+  def task_params
+    params.require(:task).permit(:content)
+  end
