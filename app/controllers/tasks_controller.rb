a03766208ee1a6921a67c89a8a59b702f1a8029b
taro.kirameki <taro.kirameki@techacademy.jp>
2020-08-14 05:54:37 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+  
+  before_action:set_task,only:[:show,:edit,:update,:destroy]
+  
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -24,11 +26,9 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-     @task = Task.find(params[:id])
 
   if @task.update(task_params)
     flash[:success] = 'タスクが更新されました'
@@ -40,7 +40,6 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
 
     flash[:success] = 'タスクは正常に削除されました'
@@ -48,6 +47,10 @@ class TasksController < ApplicationController
   end 
     
   private
+  
+  def set_task
+    @task=task.find(param[:id])
+  end
   
   def task_params
     params.require(:task).permit(:content)
